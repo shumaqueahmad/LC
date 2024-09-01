b3f4452a6da8bd906cbe6c1ffb06a659d07020f4
Shumaque Ahmad <112777904+shumaqueahmad@users.noreply.github.com>
2024-09-01 17:44:30 +0530

    Time: 3 ms (57.98%), Space: 9.4 MB (31.21%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.cpp
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.cpp
@@ -3,19 +3,41 @@ public:
     int countGoodSubstrings(string s) {
         int count=0; 
         int n=s.length();
+        int l=0, r=0;
+        unordered_map<char,int> mp;//int->freq
+        while(r<n){
+           char c=s[r];
+           mp[c]++;
 
-        for(int i=0; i<n; i++){
-            unordered_set<char> st;
-            for(int j=i; j<n; j++){
-                st.insert(s[j]);
+            if(r-l+1==3){
 
-                if(st.size()==3 && j-i+1==3){
+                if(mp.size()==3){
                     count++;
                 }
-                else if(st.size()>3) break;
-                else continue;
+            
+                    mp[s[l]]--;
+                    if(mp[s[l]]==0) mp.erase(s[l]);
+                
+                    l++;
             }
+
+
+           r++;
         }
         return count;
+
+        // for(int i=0; i<n; i++){
+        //     unordered_set<char> st;
+        //     for(int j=i; j<n; j++){
+        //         st.insert(s[j]);
+
+        //         if(st.size()==3 && j-i+1==3){
+        //             count++;
+        //         }
+        //         else if(st.size()>3) break;
+        //         else continue;
+        //     }
+        // }
+        
     }
 };
