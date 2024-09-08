560d236da7b92860ac46a3b9412bf27ef66d1163
Shumaque Ahmad <112777904+shumaqueahmad@users.noreply.github.com>
2024-09-08 22:12:09 +0530

    Time: 3 ms (83.53%), Space: 23.8 MB (93.82%) - LeetHub

--- 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.cpp
+++ 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.cpp
@@ -2,13 +2,23 @@ class Solution {
 public:
     int countPairs(vector<int>& nums, int target) {
         int count=0;
-        for(int i=0; i<nums.size(); i++){
-            for(int j=i+1; j<nums.size(); j++){
-                if(nums[i]+nums[j]<target){
-                    count++;
-                }
+        sort(nums.begin(), nums.end());
+        int l=0, r=nums.size()-1;
+
+        while(l<r){
+            if(nums[l]+nums[r]<target){
+                count+=r-l;
+                l++;
             }
+            else r--;
         }
-        return count;
+        return count; //TC O(NLONGN)+ O(N)-> O(NLOGN)
     }
 };
+ // for(int i=0; i<nums.size(); i++){  BRUTE FORCE O(N^2)
+        //     for(int j=i+1; j<nums.size(); j++){
+        //         if(nums[i]+nums[j]<target){
+        //             count++;
+        //         }
+        //     }
+        // }
