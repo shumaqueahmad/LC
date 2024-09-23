a2ff9030da18f594ce3811ac000a542fafb32576
Shumaque Ahmad <112777904+shumaqueahmad@users.noreply.github.com>
2024-09-23 14:20:03 +0530

    Time: 12 ms (29.17%), Space: 43 MB (5.56%) - LeetHub

--- 3162-find-the-number-of-good-pairs-i/3162-find-the-number-of-good-pairs-i.cpp
+++ 3162-find-the-number-of-good-pairs-i/3162-find-the-number-of-good-pairs-i.cpp
@@ -1,15 +1,30 @@
 class Solution {
 public:
     int numberOfPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-        int pairs=0;
-        for(int j=0; j<nums1.size(); j++){
-            for(int i=0; i<nums2.size(); i++){
-            int div=nums2[i]*k;
-            if(nums1[j]%div==0){
-                pairs++;
-             }
+       
+        unordered_map<int,int> freq;
+        for(int num:nums2){
+            freq[num*k]++;
+        }
+        int count=0;
+        for(const auto& n: nums1){
+            for(const auto& x:freq){
+                if(n%x.first==0){
+                    count+=x.second;
+                }
             }
         }
-        return pairs;
+        return count;
+       
+        // int pairs=0;
+        // for(int j=0; j<nums1.size(); j++){
+        //     for(int i=0; i<nums2.size(); i++){
+        //     int div=nums2[i]*k;
+        //     if(nums1[j]%div==0){
+        //         pairs++;
+        //      }
+        //     }
+        // }
+        // return pairs;
     }
 };
