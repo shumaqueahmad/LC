39cd002d57c720cf49ddddd4d21ee2308edef06e
Shumaque Ahmad <112777904+shumaqueahmad@users.noreply.github.com>
2024-10-27 07:29:23 +0530

    Time: 2 ms (49.66%), Space: 8.8 MB (98.16%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -14,15 +14,31 @@ public:
 
         func(index+1, n, nums,unique,temp);
     }
+    void func1(int ind,vector<int>& nums,vector<int>& temp,vector<vector<int>>& ans ){
+        
+        ans.push_back(temp);
+            
+        for(int i=ind; i<nums.size(); i++){
+            if(i!=ind && nums[i]==nums[i-1]) continue;
+            temp.push_back(nums[i]);
+            func1(i+1,nums,temp,ans);
+            temp.pop_back();
+        }
+    }
+    //new aproach using 
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         set<vector<int>> unique;
         int n=nums.size();
         sort(nums.begin(), nums.end());
         vector<int> temp;
-        //1. generating all subsets
-        func(0,n, nums,unique,temp);
+        vector<vector<int>> ans;
+        func1(0,nums,temp,ans);
+        return ans;
 
-        return vector<vector<int>>(unique.begin(), unique.end());
+        //1. generating all subsets
+        // func(0,n, nums,unique,temp);
+
+        // return vector<vector<int>>(unique.begin(), unique.end());
 
     }
 };
