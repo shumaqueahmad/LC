5ab49efb88ad47d1eb9ece5e6b8ce378e7f96e19
Shumaque Ahmad <112777904+shumaqueahmad@users.noreply.github.com>
2025-01-15 00:13:15 +0530

    Time: 0 ms (100%), Space: 10.2 MB (89.55%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -19,9 +19,30 @@ public:
         ans.push_back(root->val);
         func(root->right, ans);
     }
+    vector<int> itrin(TreeNode * root, vector<int> ans){
+        if(root==NULL) return ans;
+        stack<TreeNode*> st;
+        TreeNode* temp= root;
+        while(true){
+            if(temp !=NULL){
+                st.push(temp);
+                temp=temp->left;
+            }
+            else{
+                if(st.empty()==true) break;
+                temp=st.top();
+                ans.push_back(temp->val);
+                st.pop();
+
+                temp=temp->right;
+            }
+        }
+        
+        return ans;
+    }
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> ans;
-        func(root,ans);
-        return ans;
+        //func(root,ans);
+        return itrin(root,ans);
     }
 };
