b3a059187d42603aa8655ae94f2320671e457585
Shumaque Ahmad <112777904+shumaqueahmad@users.noreply.github.com>
2025-08-01 14:12:09 +0530

    Time: 4 ms (64.92%), Space: 15.5 MB (49.68%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
@@ -10,56 +10,54 @@
 class Solution {
 public:
     void markparent(TreeNode* root,unordered_map<TreeNode*, TreeNode*>& parent){
-        //level order to mark parents in map
-        queue<TreeNode*>q;
+        queue<TreeNode*> q;
         q.push(root);
         while(!q.empty()){
-            TreeNode* temp=q.front();
+            auto temp=q.front();
             q.pop();
             if(temp->left){
-                parent[temp->left]= temp;
                 q.push(temp->left);
+                parent[temp->left]=temp;
             }
             if(temp->right){
-                parent[temp->right]= temp;
                 q.push(temp->right);
+                parent[temp->right]=temp;
             }
         }
     }
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        unordered_map<TreeNode*, TreeNode*> parent;
+        vector<int> ans;
+        if(root==NULL) return ans;
+        unordered_map<TreeNode*, TreeNode*>parent;
         markparent(root,parent);
-        int count=0;
 
-        //creating a visited map
-        unordered_map<TreeNode*,int>visit;
         queue<TreeNode*>q;
+        unordered_map<TreeNode*, int>m1;
         q.push(target);
-        visit[target]=1;
+        m1[target]=1;
+        int count=0;
         while(!q.empty()){
             int n=q.size();
             if(count==k) break;
-            count++; //incrementing count as now i am going to move 1 step away from target
-            
+            count++;
             for(int i=0; i<n; i++){
-                TreeNode* temp=q.front();
+                auto temp=q.front();
                 q.pop();
-
-                if(temp->left && visit[temp->left]!=1){
-                    visit[temp->left]=1;
+                if(temp->left!=NULL && m1[temp->left]!=1){
                     q.push(temp->left);
+                    m1[temp->left]=1;
                 }
-                if(temp->right && visit[temp->right]!=1){
-                    visit[temp->right]=1;
+                if(temp->right!=NULL && m1[temp->right]!=1){
                     q.push(temp->right);
+                    m1[temp->right]=1;
                 }
-                if(parent[temp] && visit[parent[temp]]!=1){
-                    visit[parent[temp]]=1;
+                if(parent[temp]!=NULL && m1[parent[temp]]!=1){
                     q.push(parent[temp]);
+                    m1[parent[temp]]=1;
                 }
             }
         }
-        vector<int> ans;
+
         while(!q.empty()){
             ans.push_back(q.front()->val);
             q.pop();
