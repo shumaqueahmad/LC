0ab3370a57c2c504e309ba2a601aea3c39586d63
Shumaque Ahmad <112777904+shumaqueahmad@users.noreply.github.com>
2025-08-11 01:37:59 +0530

    Time: 0 ms (100%), Space: 24 MB (9.92%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -4,32 +4,19 @@ public:
         // instead of sorting, approach is to find the first zero element and
         // swap it with a non zero element
         int n = nums.size();
-        /*int j = -1;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == 0) {
-                j = i;
+        int j=-1; //tracks zero elem
+        for(int i=0; i<n; i++){
+            if(nums[i]==0){
+                j=i;
                 break;
             }
         }
-
-        if (j == -1) {
-            return;
-        }
-
-        for (int i = j + 1; i < n; i++) {
-            if (nums[i] != 0) {
-                swap(nums[j], nums[i]);
+        if(j==-1) return;
+        for(int i=j+1; i<n; i++){
+            if(nums[i]!=0){
+                swap(nums[i], nums[j]);
                 j++;
             }
-        }*/
-
-        for(int i=0; i<n-1; i++){
-            for(int j=0; j<n-1-i; j++){
-                if(nums[j]==0){
-                    swap(nums[j],nums[j+1]);
-                }
-            }
         }
-        
     }
 };
